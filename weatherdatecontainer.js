3269593640cd19a239ef22c17dcc9e22202d3fa8
Man Tin <123168693+mtw72@users.noreply.github.com>
2023-06-14 22:25:26 +0100

    Add files via upload

--- weatherdatecontainer.js
+++ weatherdatecontainer.js
@@ -1,24 +1,25 @@
 // Date div
 const todayDiv = document.querySelector('.today');
-const threedayslaterDiv = document.querySelector('.three-days-later');
+const tomorrowDiv = document.querySelector('.tomorrow');
+const twoDaysLaterDiv = document.querySelector('.two-days-later');
+const threeDaysLaterDiv = document.querySelector('.three-days-later');
 const currentTimeOfToday = new Date().getHours();
 
 if (currentTimeOfToday < 3) {
     todayDiv.style.width = '420px';
-    threedayslaterDiv.style.display = 'none';
 } else if (currentTimeOfToday < 6) {
     todayDiv.style.width = '364px';
-    threedayslaterDiv.style.display = 'none';
 } else if (currentTimeOfToday < 9) {
-    todayDiv.style.width = '317px';
+    todayDiv.style.width = '315px';
 } else if (currentTimeOfToday < 12) {
     todayDiv.style.width = '262px';
 } else if (currentTimeOfToday < 15) {
-    todayDiv.style.width = '208px';
+    todayDiv.style.width = '209px';
 } else if (currentTimeOfToday < 18) {
     todayDiv.style.width = '156px';
 } else if (currentTimeOfToday < 21) {
     todayDiv.style.width = '104px';
 } else {
-    todayDiv.style.width = '52px';
+    todayDiv.style.width = '54px';
+    tomorrowDiv.style.width = '415px';
 }
